c4a028e320276fd71ec6f83a02e1c52926a2e000
yf <yufeng200092@qq.com>
2017-05-31 22:46:01 +0800

    增加现金流，增加保存收益

--- YieldCalc/Form1.Designer.cs
+++ YieldCalc/Form1.Designer.cs
@@ -28,36 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colMoney = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btnCalc = new System.Windows.Forms.Button();
-            this.txtReturn = new System.Windows.Forms.TextBox();
-            this.txtResult = new System.Windows.Forms.TextBox();
             this.txtDate = new System.Windows.Forms.DateTimePicker();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtReturn = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMoney = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 240F));
+            this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(548, 323);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 59F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(551, 356);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // dataGridView1
@@ -70,82 +78,139 @@
             this.dataGridView1.Location = new System.Drawing.Point(3, 3);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(542, 234);
+            this.dataGridView1.Size = new System.Drawing.Size(545, 210);
             this.dataGridView1.TabIndex = 0;
             // 
             // Date
             // 
             this.Date.DataPropertyName = "Date";
-            dataGridViewCellStyle3.Format = "yyyy/MM/dd";
-            dataGridViewCellStyle3.NullValue = null;
-            this.Date.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle5.Format = "yyyy/MM/dd";
+            dataGridViewCellStyle5.NullValue = null;
+            this.Date.DefaultCellStyle = dataGridViewCellStyle5;
             this.Date.HeaderText = "日期";
             this.Date.Name = "Date";
             // 
             // colMoney
             // 
             this.colMoney.DataPropertyName = "Money";
-            dataGridViewCellStyle4.Format = "N2";
-            dataGridViewCellStyle4.NullValue = "0";
-            this.colMoney.DefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle6.Format = "N2";
+            dataGridViewCellStyle6.NullValue = "0";
+            this.colMoney.DefaultCellStyle = dataGridViewCellStyle6;
             this.colMoney.HeaderText = "金额";
             this.colMoney.Name = "colMoney";
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.txtDate);
-            this.groupBox1.Controls.Add(this.txtResult);
             this.groupBox1.Controls.Add(this.txtReturn);
             this.groupBox1.Controls.Add(this.btnCalc);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(3, 243);
+            this.groupBox1.Location = new System.Drawing.Point(3, 219);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(542, 77);
+            this.groupBox1.Size = new System.Drawing.Size(545, 53);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            // 
-            // btnCalc
-            // 
-            this.btnCalc.Location = new System.Drawing.Point(392, 32);
-            this.btnCalc.Name = "btnCalc";
-            this.btnCalc.Size = new System.Drawing.Size(61, 23);
-            this.btnCalc.TabIndex = 0;
-            this.btnCalc.Text = "计算";
-            this.btnCalc.UseVisualStyleBackColor = true;
-            this.btnCalc.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // txtReturn
-            // 
-            this.txtReturn.Location = new System.Drawing.Point(49, 32);
-            this.txtReturn.Name = "txtReturn";
-            this.txtReturn.Size = new System.Drawing.Size(100, 21);
-            this.txtReturn.TabIndex = 1;
-            this.txtReturn.Text = "0.00";
-            // 
-            // txtResult
-            // 
-            this.txtResult.Location = new System.Drawing.Point(459, 34);
-            this.txtResult.Name = "txtResult";
-            this.txtResult.ReadOnly = true;
-            this.txtResult.Size = new System.Drawing.Size(65, 21);
-            this.txtResult.TabIndex = 2;
             // 
             // txtDate
             // 
             this.txtDate.CustomFormat = "yyyy/MM/dd";
             this.txtDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.txtDate.Location = new System.Drawing.Point(200, 32);
+            this.txtDate.Location = new System.Drawing.Point(77, 17);
             this.txtDate.Name = "txtDate";
             this.txtDate.Size = new System.Drawing.Size(122, 21);
-            this.txtDate.TabIndex = 4;
+            this.txtDate.TabIndex = 0;
             this.txtDate.Value = new System.DateTime(2017, 5, 27, 11, 33, 44, 0);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(471, 11);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(65, 21);
+            this.txtResult.TabIndex = 2;
+            // 
+            // txtReturn
+            // 
+            this.txtReturn.Location = new System.Drawing.Point(270, 17);
+            this.txtReturn.Name = "txtReturn";
+            this.txtReturn.Size = new System.Drawing.Size(100, 21);
+            this.txtReturn.TabIndex = 1;
+            this.txtReturn.Text = "0.00";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(475, 15);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(61, 23);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "计算";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtMoney);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtResult);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 278);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(545, 75);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "日期";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "收益";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(400, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "年化收益率";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "总现金流";
+            // 
+            // txtMoney
+            // 
+            this.txtMoney.Location = new System.Drawing.Point(77, 11);
+            this.txtMoney.Name = "txtMoney";
+            this.txtMoney.ReadOnly = true;
+            this.txtMoney.Size = new System.Drawing.Size(65, 21);
+            this.txtMoney.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(548, 323);
+            this.ClientSize = new System.Drawing.Size(551, 356);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "收益率计算器";
@@ -153,6 +218,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -168,6 +235,12 @@
         private System.Windows.Forms.TextBox txtResult;
         private System.Windows.Forms.TextBox txtReturn;
         private System.Windows.Forms.DateTimePicker txtDate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtMoney;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- YieldCalc/Form1.cs
+++ YieldCalc/Form1.cs
@@ -13,26 +13,36 @@ namespace YieldCalc
 {
     public partial class Form1 : Form
     {
-        private DataTable dtData;
+        private const string Rate = "Rate";
+        private const string Return = "Return";
+        private DataSet ds;
         public Form1()
         {
             InitializeComponent();
-            dtData = new DataTable();
-            dtData.TableName = "Rate";
             this.txtDate.Value = DateTime.Today;
+            ds = new DataSet("Data");
+
+            DataTable dtData = new DataTable(Rate);
             dtData.Columns.Add("Date", typeof(DateTime));
             dtData.Columns.Add("Money", typeof(double));
+            DataTable dtReturn = new DataTable(Return);
+            dtReturn.Columns.Add("Value", typeof(double));
+
+            ds.Tables.Add(dtData);
+            ds.Tables.Add(dtReturn);
 
             try
             {
-                dtData.ReadXml(Path.Combine(Environment.CurrentDirectory, "Data.xml"));
+                ds.ReadXml(Path.Combine(Environment.CurrentDirectory, "Data.xml"));
             }
-            catch
+            catch(Exception ex)
             {
 
+                dtReturn.Rows.Add(0);
             }
 
-            this.dataGridView1.DataSource = dtData;
+            this.dataGridView1.DataSource = ds.Tables[Rate];
+            this.txtReturn.Text = ds.Tables[Return].Rows[0][0].ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -45,32 +55,61 @@ namespace YieldCalc
             }
 
             DateTime dtEndDate = this.txtDate.Value;
+            DataTable dtData = ds.Tables[0];
             if (dtData.Rows.Count > 0)
             {
-                double sumMul = 0;
-                DataRow[] rows = dtData.Select(null, "Date");
-                foreach (DataRow row in rows)
-                {
-                    DateTime date = (DateTime)row["Date"];
-                    double money = (double)row["Money"];
-
-                    TimeSpan span = dtEndDate - date;
-                    int dates = (int)span.TotalDays;
-                    double timeMoney = dates * money;
-                    sumMul += timeMoney;
-                }
-
-                DateTime firstDate = (DateTime)rows[0]["Date"];
-                double firstTotalDay = (dtEndDate - firstDate).TotalDays;
-                double conMoney = sumMul / firstTotalDay;
-
-                //double rateDates = decReturn / firstTotalDay / conMoney;
-
-                double countToYear = 365 / firstTotalDay;
-                double result = Math.Pow(1 + decReturn / conMoney, countToYear) - 1;
+                double result = CalcYield(decReturn, dtEndDate);
+                double money = CalcMoney();
                 this.txtResult.Text = result.ToString("p");
-                dtData.WriteXml(Path.Combine(Environment.CurrentDirectory, "Data.xml"), XmlWriteMode.IgnoreSchema);
+                this.txtMoney.Text = money.ToString("N2");
+
+                ds.Tables[Return].Rows[0][0] = decReturn;
+                ds.WriteXml(Path.Combine(Environment.CurrentDirectory, "Data.xml"));
             }
+        }
+
+
+        private double CalcYield(double decReturn, DateTime dtEndDate)
+        {
+            double sumMul = 0;
+            DataTable dtData = ds.Tables[0];
+            DataRow[] rows = dtData.Select(null, "Date");
+            foreach (DataRow row in rows)
+            {
+                DateTime date = (DateTime)row["Date"];
+                double money = (double)row["Money"];
+
+                TimeSpan span = dtEndDate - date;
+                int dates = (int)span.TotalDays;
+                double timeMoney = dates * money;
+                sumMul += timeMoney;
+            }
+
+            DateTime firstDate = (DateTime)rows[0]["Date"];
+            double firstTotalDay = (dtEndDate - firstDate).TotalDays;
+            double conMoney = sumMul / firstTotalDay;
+
+            //double rateDates = decReturn / firstTotalDay / conMoney;
+
+            double countToYear = 365 / firstTotalDay;
+            double result = Math.Pow(1 + decReturn / conMoney, countToYear) - 1;
+            return result;
+        }
+
+        private double CalcMoney()
+        {
+            double sumMoney = 0;
+            DataTable dtData = ds.Tables[0];
+            DataRow[] rows = dtData.Select(null, "Date");
+            foreach (DataRow row in rows)
+            {
+                DateTime date = (DateTime)row["Date"];
+                double money = (double)row["Money"];
+
+                sumMoney += money;
+            }
+
+            return sumMoney;
         }
     }
 }
